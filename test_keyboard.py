6f91342d9b9bf131c07e6312ccc45e8a33a88f1d
Julien Bergerot <julien.bergerot@polytechnique.edu>
2024-04-30 13:11:17 +0200

    feat: more tests

--- test_keyboard.py
+++ test_keyboard.py
@@ -1,8 +1,20 @@
 import keyboard
 
+TEST = 1
 
-while True:
-    # Wait for the next event.
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN:
-        print(event.name)  # to check key name
+if TEST == 1:
+    while True:
+        # Wait for the next event.
+        event = keyboard.read_event()
+        if event.event_type == keyboard.KEY_DOWN:
+            print(event.name)  # to check key name
+
+if TEST == 2:
+    while True:  # making a loop
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed("v"):  # if key 'q' is pressed
+                print("You Pressed V Key!")
+                break  # finishing the loop
+        except:
+            print("Error")
+            break
